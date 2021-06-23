8aa069f776def069ebbf27de5346f5dad6b56180
zakaria <zakariaselassi@gmail.com>
2021-06-23 16:15:05 +0200

    update : geolaction modifcation

--- client/src/App.js
+++ client/src/App.js
@@ -5,54 +5,20 @@ import {MapContainer, TileLayer, Marker, Popup,useMap } from 'react-leaflet'
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css'
 import icon from "./components/Icone";
-import useGeoLocation from "./components/useGeoLocation";
-/*delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
-});*/
-function LocationMarker() {
-  const [position, setPosition] = useState(null);
-  const [bbox, setBbox] = useState([]);
-  const map = useMap();
-  useEffect(() => {
-    map.locate().on("locationfound", function (e) {
-      setPosition(e.latlng);
-
-    });
-  }, [map]);
-
-  return position === null ? null : (
-    <Marker position={position} icon={icon}>
-      <Popup>
-        You are here. <br />
-        Map bbox: <br />
-        <b>Southwest lng</b>: {bbox[0]} <br />
-        <b>Southwest lat</b>: {bbox[1]} <br />
-        <b>Northeast lng</b>: {bbox[2]} <br />
-        <b>Northeast lat</b>: {bbox[3]}
-      </Popup>
-    </Marker>
-  );
-}
-
-
+import LocationMarker from "./components/LocationMarker";
 
 function App() {
     const [items,setItems] = useState([]);
-    useEffect(() => {
-      
+    useEffect(() => {  
       const fetching = async () => {
         const response = await axios.get('http://localhost:3001/read')
         console.log(response.data);
         return setItems(response.data)
       }
-   
       fetching()
     },[])
-  
-  return (
+
+    return (
     <>
       <MapContainer center={[50.4167 ,4.4333]} zoom={15} scrollWheelZoom={true}>
         <TileLayer
---
+++ client/src/components/LocationMarker.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect,useRef } from "react";
+import axios from 'axios'
+import {MapContainer, TileLayer, Marker, Popup,useMap } from 'react-leaflet'
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css'
+import icon from "./Icone";
+
+function LocationMarker() {
+  const [position, setPosition] = useState(null);
+  const [bbox, setBbox] = useState([]);
+
+  const map = useMap();
+
+  useEffect(() => {
+    map.locate().on("locationfound", function (e) {
+      setPosition(e.latlng);
+      map.flyTo(e.latlng, map.getZoom());
+      const radius = e.accuracy;
+      const circle = L.circle(e.latlng, radius);
+      circle.addTo(map);
+      setBbox(e.bounds.toBBoxString().split(","));
+    });
+  }, [map]);
+
+  return position === null ? null : (
+    <Marker position={position} icon={icon}>
+      <Popup>
+        You are here. <br />
+        Map bbox: <br />
+        <b>Southwest lng</b>: {bbox[0]} <br />
+        <b>Southwest lat</b>: {bbox[1]} <br />
+        <b>Northeast lng</b>: {bbox[2]} <br />
+        <b>Northeast lat</b>: {bbox[3]}
+      </Popup>
+    </Marker>
+  );
+}
+  
+
+export default LocationMarker;
