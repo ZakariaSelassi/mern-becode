59ad49949f7d6a34935df62102ad574cb6de72c7
zakaria <zakariaselassi@gmail.com>
2021-06-23 15:27:57 +0200

    update : current position + display all terminals

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,73 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect,useRef } from "react";
 import './App.css'
 import axios from 'axios'
-import {MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import {MapContainer, TileLayer, Marker, Popup,useMap } from 'react-leaflet'
 import L from 'leaflet';
+import 'leaflet/dist/leaflet.css'
+import icon from "./components/Icone";
+import useGeoLocation from "./components/useGeoLocation";
+/*delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});*/
+function LocationMarker() {
+  const [position, setPosition] = useState(null);
+  const [bbox, setBbox] = useState([]);
+  const map = useMap();
+  useEffect(() => {
+    map.locate().on("locationfound", function (e) {
+      setPosition(e.latlng);
+
+    });
+  }, [map]);
+
+  return position === null ? null : (
+    <Marker position={position} icon={icon}>
+      <Popup>
+        You are here. <br />
+        Map bbox: <br />
+        <b>Southwest lng</b>: {bbox[0]} <br />
+        <b>Southwest lat</b>: {bbox[1]} <br />
+        <b>Northeast lng</b>: {bbox[2]} <br />
+        <b>Northeast lat</b>: {bbox[3]}
+      </Popup>
+    </Marker>
+  );
+}
+
+
+
 function App() {
-    const [items,setItems] = useState({});
-    const [center,setCenter] = useState({lat:50, lng:4});
-    const maptiler = {
-      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-    };
-    
+    const [items,setItems] = useState([]);
     useEffect(() => {
       
       const fetching = async () => {
-        let fullData = {};
-        const res = await fetch('http://localhost:3001/read')
-        let dater = await res.json();
-        dater.map(item => {
-          const {address,bank,latitude,longitude} = item
-          fullData[bank] = {address,bank,latitude,longitude}
-        })
-        return setItems(fullData)
+        const response = await axios.get('http://localhost:3001/read')
+        console.log(response.data);
+        return setItems(response.data)
       }
    
       fetching()
     },[])
-    console.log(items)
   
   return (
     <>
-      <MapContainer center={[50.6318,3.77575]} zoom={13} scrollWheelZoom={true}>
+      <MapContainer center={[50.4167 ,4.4333]} zoom={15} scrollWheelZoom={true}>
         <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
         {
-          Object.values(items).map((data,index) => {
-            <Marker key={index} position={data.latitude,data.longitude}>
-              <div className="marker" style={{height:500,width:500}}>ICI</div>
+          items.map((terminal,index) => (
+            <Marker key={index} position={[terminal.latitude, terminal.longitude]}>
               <Popup>
                 <p>ICI</p>
               </Popup>
             </Marker>
-          })
-        }
-       
-
+          ))
+          }
+          <LocationMarker/>
       </MapContainer>
     </>
   );
